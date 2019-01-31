6ec1c2da33d0821551ac9751c1c81369b8476ec7
gillharman1991 <harman.gill018@gmail.com>
2019-01-31 11:45:16 -0500

    created funtion and loaded with body onload event

--- co742456/js/main.js
+++ co742456/js/main.js
@@ -1,1 +1,6 @@
-﻿// Your code here!
+﻿function datatest()
+{
+    var date = new Date
+    document.body.innerHTML = "<h1>the today  date is " + date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>"
+
+}
